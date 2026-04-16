c3fee29e1e3aa4ee2b11464a23bf18fa0d10a89b
Mardiansyah <mardiansyahiyan2005@gmail.com>
2026-04-16 20:20:14 +0700

    Muhammad Mardiansyah_Praktikum_Git

---
+++ Muhammad-Mardiansyah.php
@@ -0,0 +1,14 @@
+<?php
+
+$nama = "Muhammad Mardiansyah";
+$umur = 21;
+$alamat = "Jawa Timur";
+$hobi = "Ngoding, Networking, Gaming";
+
+
+echo "<h1>Perkenalan Diri</h1>";
+echo "<p>Halo, nama saya <b>$nama</b>.</p>";
+echo "<p>Umur saya $umur tahun.</p>";
+echo "<p>Saya tinggal di $alamat.</p>";
+echo "<p>Hobi saya adalah $hobi.</p>";
+?>
